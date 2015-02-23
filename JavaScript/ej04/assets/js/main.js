d63588647a69d3c65b54fb9032ebeec4a380ab41
markelor <mendimarkel@gmail.com>
2015-02-23 17:36:03 +0100

    JS ejercicio 04

---
+++ JavaScript/ej04/assets/js/main.js
@@ -0,0 +1,26 @@
+(function() {
+
+
+    //par o impar
+
+    var esPar = function(num) {
+        var par = true;
+        if(n&& typeof n==="number"){
+            par="escribe un numero";
+            }
+
+        if (num % 2 === 0) {
+            par = true;
+        } else {
+            par = false;
+        }
+
+        return par;
+    };
+
+    console.log(esPar(34));
+     console.log(esPar(35));
+      console.log(esPar("a"));
+
+
+})();
