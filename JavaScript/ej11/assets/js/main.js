eb13030cc1375ba459c3345b82773d23972bd55e
markelor <mendimarkel@gmail.com>
2015-02-25 19:29:58 +0100

    JS ejercicio 11

--- JavaScript/ej11/assets/js/main.js
+++ JavaScript/ej11/assets/js/main.js
@@ -1,20 +1,36 @@
 window.onload = function() {
     var ul = document.getElementById("lista");
     btn = document.getElementById("add");
-        //ultimoLi=document.getElementsById("boton");
 
-    var add = function() {
+
+    var add = function(e) {
+        e.stopPropagation();
         if (ul) {
-
 
             var li = document.createElement("li");
             li.innerText = "Elemento" + (ul.children.length + 1);
             ul.appendChild(li);
         }
     };
+    var remove = function(e) {
+        if (confirm("Seguro que deseas eliminar?")) {
+            this.removeChild(e.target);
+            console.log(this);
+        }
+    };
+
+    var log = function(e) {
+        console.log(e);
+
+
+
+    };
     if (btn) {
         btn.addEventListener("click", add, false);
+
     }
-
-
+    if (ul) {
+        ul.addEventListener("click", remove, false);
+    }
+    document.addEventListener("click", log, false);
 };
