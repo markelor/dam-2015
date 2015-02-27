5b9fb1cd6b3e5041c48e21ae2ea21eb63664b1cd
markelor <mendimarkel@gmail.com>
2015-02-27 17:43:42 +0100

    Angular ejercicio 01

---
+++ Angular/ej01/assets/js/main.js
@@ -0,0 +1,12 @@
+var todoApp=angular.module("todoApp", []);
+todoApp.controller("TodoController",["$scope",function($scope){
+    var todos=[];
+    todos.push({done:false,task:"Aprender AngularJS"});
+    todos.push({done:false,task:"Aprender Ionic"});
+    todos.push({done:true,task:"Aprender JavaScript"});
+    $scope.todos=todos;
+    $scope.addTodo=function(){
+        todos.push({done:false,task:$scope.inputText});
+        $scope.inputText="";
+    };
+}]);
